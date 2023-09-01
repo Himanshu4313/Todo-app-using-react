fd55f5af088e3255e9eabea7157240a6c9d848ea
Himanshu4313 <hk225064@gmail.com>
2023-09-01 16:01:20 +0530

    add-input-box part style complete

--- src/Components/AddTodo/AddToDo.jsx
+++ src/Components/AddTodo/AddToDo.jsx
@@ -1,25 +1,30 @@
 import { useContext, useState } from "react";
 import DispatchContextapi from "../../Contextapi/DispatchContextapi";
-
+import './AddToDo.css';
 //Add todo component
 const AddTodo = () => {
-  const [inputText , setInputText] = useState('');
+  const [inputText, setInputText] = useState('');
   //WE use dispatch from dispatch contextapi and this dispatch context api store only dispatch method.
-  const {dispatch} = useContext(DispatchContextapi);
+  const { dispatch } = useContext(DispatchContextapi);
   return (
     <>
-         <div>
-            <input type="text"
-             value={inputText}
-             placeholder="add your next todo..."
-             onChange={(e) => setInputText(e.target.value)}
-             />
-            <button onClick={() =>{
-               dispatch({type : 'add_todo', payload : {todoText:inputText}});
-               setInputText('');
-            }
-               }> Add </button>
-         </div>
+      <div className="add-todo-container">
+        <div>
+          <h1 className="Heading">My Todos</h1>
+           <input type="text"
+            value={inputText}
+            placeholder="add your next todo..."
+            onChange={(e) => setInputText(e.target.value)}
+            id="Addinputbox"
+          />
+          <button onClick={() => {
+            dispatch({ type: 'add_todo', payload: { todoText: inputText } });
+            setInputText('');
+          }
+          }> Add </button>
+        </div>
+
+      </div>
     </>
   );
 }
