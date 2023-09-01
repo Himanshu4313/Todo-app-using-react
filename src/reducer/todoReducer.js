ef8143e776e04067081bd2801492dded1cc86a55
Himanshu4313 <hk225064@gmail.com>
2023-09-01 12:17:16 +0530

    add reducer folder

---
+++ src/reducer/todoReducer.js
@@ -0,0 +1,12 @@
+//todo reducer function 
+const TodoReducer = (state , action) => {
+    if(action == 'add_todo'){
+
+    }else if(action == 'edit_todo'){
+
+    }else if(action == 'delete_todo'){
+
+    }
+         
+}
+export default TodoReducer;
